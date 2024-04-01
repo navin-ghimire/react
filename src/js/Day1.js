8580e596eb3518f45eb1c3c79f2f86424add06a7
navin-ghimire <navinghimire143@gmail.com>
2024-04-01 13:05:25 +0545

    ch

---
+++ src/js/Day1.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// const Day1 = () => {
+
+//   const age = 90;
+//   const newTitle = 'Today News';
+//   const calcage = () => {
+//     return age + 100;
+//   }
+
+//   return (
+//     <div>
+
+//       <h1>{calcage()}</h1>
+//       <h3>{age > 50 ? 'senior' : 'junior'}</h3>
+//       <h1>{newTitle} {age}</h1>
+
+//     </div>
+//   )
+// }
+
+// export default Day1

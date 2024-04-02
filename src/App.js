e84f44c7c90fc14293c6ab3e97801391c2b490b8
navin-ghimire <navinghimire143@gmail.com>
2024-04-02 21:31:36 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import Footer from './ui/Footer'
-import Header from './ui/Header'
-import Homepages from './features/home/Homepages'
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <div className='border-b-2 border-black'></div>
-      <Homepages />
-      <div className='border-b-2 border-black'></div>
-      <Footer />
+
     </div>
   )
 }

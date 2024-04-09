bc33346c75e44c1f59637c88799523caca41a0af
navin-ghimire <navinghimire143@gmail.com>
2024-04-09 22:06:35 +0545

    change

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,15 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import Sample from './Sample';
+import React, { useEffect, useRef } from 'react'
 
 const Home = () => {
-  const [show, setshow] = useState(true);
 
+  const tar = useRef(null);
+  // useEffect(() => {
+  //   tar.current.focus();
+  // }, [])
 
+  const goDown = () => {
+    tar.current.scrollIntoView({ behavior: 'smooth'});
+  }
+  
+
+ 
   return (
     <div>
-      <button onClick={() => setshow(!show)}>Toggle</button>
+      <button onClick={goDown}><h1>Go To 7</h1></button>
+      {/* <input ref={tar} className='border-2 border-black' type='text'/>
+       */}
 
-      {show && <Sample show={show}/>}
+       <div className='bx1'>
+        <h1>box1</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nam cupiditate aspernatur hic libero ipsam consequatur quis mollitia tenetur, consequuntur, enim distinctio laborum dolore quam maiores possimus blanditiis omnis beatae est adipisci ut facere natus. Dolores, atque fuga dolorem illum rerum dolore dicta ducimus culpa animi id explicabo quae quod!
+       </div>
+       <div className='bx1'>
+        <h1>box1</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nam cupiditate aspernatur hic libero ipsam consequatur quis mollitia tenetur, consequuntur, enim distinctio laborum dolore quam maiores possimus blanditiis omnis beatae est adipisci ut facere natus. Dolores, atque fuga dolorem illum rerum dolore dicta ducimus culpa animi id explicabo quae quod!
+       </div>
+       <div className='bx1'>
+        <h1>box1</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nam cupiditate aspernatur hic libero ipsam consequatur quis mollitia tenetur, consequuntur, enim distinctio laborum dolore quam maiores possimus blanditiis omnis beatae est adipisci ut facere natus. Dolores, atque fuga dolorem illum rerum dolore dicta ducimus culpa animi id explicabo quae quod!
+       </div>
+       <div className='bx1'>
+        <h1>box1</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nam cupiditate aspernatur hic libero ipsam consequatur quis mollitia tenetur, consequuntur, enim distinctio laborum dolore quam maiores possimus blanditiis omnis beatae est adipisci ut facere natus. Dolores, atque fuga dolorem illum rerum dolore dicta ducimus culpa animi id explicabo quae quod!
+       </div>
+       <div className='bx1'>
+        <h1>box1</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nam cupiditate aspernatur hic libero ipsam consequatur quis mollitia tenetur, consequuntur, enim distinctio laborum dolore quam maiores possimus blanditiis omnis beatae est adipisci ut facere natus. Dolores, atque fuga dolorem illum rerum dolore dicta ducimus culpa animi id explicabo quae quod!
+       </div>
+       <div className='bx1'>
+        <h1>box1</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nam cupiditate aspernatur hic libero ipsam consequatur quis mollitia tenetur, consequuntur, enim distinctio laborum dolore quam maiores possimus blanditiis omnis beatae est adipisci ut facere natus. Dolores, atque fuga dolorem illum rerum dolore dicta ducimus culpa animi id explicabo quae quod!
+       </div>
+       <div ref={tar} className='bx7'>
+        <h1>box7</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nam cupiditate aspernatur hic libero ipsam consequatur quis mollitia tenetur, consequuntur, enim distinctio laborum dolore quam maiores possimus blanditiis omnis beatae est adipisci ut facere natus. Dolores, atque fuga dolorem illum rerum dolore dicta ducimus culpa animi id explicabo quae quod!
+       </div>
     </div>
   )
 }
